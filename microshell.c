2443f2a0edc452998deae2c77d34a00ee4e2ae69
Kali <kali@kali>
2022-05-02 10:55:19 -0400

    check leaks

--- microshell.c
+++ microshell.c
@@ -2,6 +2,8 @@
 # include <string.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 
 int		g_status = 0;
 int		n_pid = 0;
@@ -111,7 +113,9 @@ void	free_ast(t_ast *ast)
 	t_ast	*tmp;
 	int		i;
 
-	while (ast->next)
+	if (!ast)
+		return ;
+	while (ast)
 	{
 		tmp = ast->next;
 		i = 0;
@@ -147,6 +151,8 @@ t_ast	*build_one_node(int *n_arg, int ac, char **av)
 	int		i;
 	t_ast	*ast;
 
+	if (*n_arg >= ac)
+		return (0);
 	ast = malloc(sizeof(t_ast));
 	if (!ast)
 		ft_error("error: fatal\n", ast);
@@ -218,6 +224,7 @@ void	close_for_zero(int **fds)
 	int		i;
 
 	i = 1;
+	close(fds[0][0]);
 	while (i < n_pid)
 	{
 		close(fds[i][0]);
@@ -250,7 +257,8 @@ void	manage_fd(int **fds, int i)
 	if (i == 0)
 	{
 		close_for_zero(fds);
-		dup2(fds[i][1], STDOUT_FILENO);
+		if (n_pid > 1)
+			dup2(fds[i][1], STDOUT_FILENO);
 		close(fds[i][1]);
 	}
 	else if (i < n_pid - 1)
@@ -273,7 +281,6 @@ void	manage_fd(int **fds, int i)
 void	ft_child(t_ast *ast, int **fds, char **envp, int i)
 {
 	manage_fd(fds, i);
-	printf("trying to execute : %s\n", ast->arg[0]);
 	if (!strcmp(ast->arg[0], "cd"))
 		ft_cd(ast, fds);
 	else if (execve(ast->arg[0], ast->arg, envp) < 0)
@@ -295,15 +302,32 @@ void	wait_pid(pid_t *child_pid)
 	}
 }
 
+void	close_fds_in_parent(int **fds, int i, int config)
+{
+	if (config == 1)
+	{
+		if (i > 2)
+			close(fds[i - 2][0]);
+		close(fds[i][1]);
+	}
+	else
+	{
+		close(fds[i][0]);
+		if (i > 1)
+			close(fds[i - 1][0]);
+	}
+}
+
 void	ft_microshell(t_ast *ast, pid_t *child_pid, int **fds, char **envp)
 {
 	t_ast	*next;
 	int		i;
 
 	next = ast;
-	i = 0;
+	i = -1;
 	while (next)
 	{
+		i++;
 		child_pid[i] = fork();
 		if (child_pid[i] == -1)
 		{
@@ -313,11 +337,11 @@ void	ft_microshell(t_ast *ast, pid_t *child_pid, int **fds, char **envp)
 		}
 		else if (child_pid[i] == 0)
 			ft_child(next, fds, envp, i);
-/*		else
-			close_fds_in_parent(fds);*/
-		i++;
+		else
+			close_fds_in_parent(fds, i, 1);
 		next = ast->next;
 	}
+	close_fds_in_parent(fds, i, 0);
 	wait_pid(child_pid);
 }
 
@@ -328,7 +352,9 @@ int	main(int ac, char **av, char **envp)
 	int		**fds;
 
 	ast = build_ast(ac, av);
-	print_ast(ast);
+	if (!n_pid)
+		return (0);
+//	print_ast(ast);
 	child_pid = malloc(sizeof(pid_t) * n_pid);
 	if (!child_pid)
 			ft_error("error: fatal\n", ast);
